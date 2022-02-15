b56035e856f7712c75b66ceabafcfc47b76c2c2f
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-02-15 21:05:40 +0100

    fixes for web sockets

--- instantpoll/asgi.py
+++ instantpoll/asgi.py
@@ -13,8 +13,10 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import apps.frontend.routing
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instantpoll.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
